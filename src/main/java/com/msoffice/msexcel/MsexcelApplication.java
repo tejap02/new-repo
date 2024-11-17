bc1feab40662e008fb3915c7e65e9d0c3f2a0232
tejap02 <patiltejaswi0@gmail.com>
2024-11-17 14:47:50 +0530

    Add files via upload

---
+++ src/main/java/com/msoffice/msexcel/MsexcelApplication.java
@@ -0,0 +1,13 @@
+package com.msoffice.msexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsexcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsexcelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msoffice/msexcel/MsexcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msoffice.msexcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsexcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
